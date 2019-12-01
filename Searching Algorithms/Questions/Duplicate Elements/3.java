bbc6c1007e98d79af1d0fc41c3f904b9c1c7aa5c
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2019-12-01 14:37:16 +0530

    Create 3.java

---
+++ Searching Algorithms/Questions/Duplicate Elements/3.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class duplicate{
+    public static void main(String[] args){
+    int n,i,j,flag=0;
+    Scanner s=new Scanner(System.in);
+    System.out.println("Enter the number of elements that you are going to enter into the array:");
+    n=s.nextInt();
+    int arr[]=new int[n];
+    System.out.println("Enter the elements into the array:");
+    for(i=0;i<n;i++){
+        arr[i]=s.nextInt();
+    }
+    
+    for(i=0;i<n;i++){
+        if(arr[Math.abs(arr[i])]<0){
+            flag=1;
+            System.out.println("Duplicate Element is "+arr[i]);
+            break;
+        }
+        else{
+            arr[arr[i]]=-arr[arr[i]];
+        }
+    }
+   
+    if(flag==0){
+        System.out.println("No Duplicates in the Array");
+    }
+}
+}
+    
+        
