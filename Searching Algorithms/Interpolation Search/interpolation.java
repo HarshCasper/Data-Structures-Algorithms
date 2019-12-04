ea272d2f97db5111a038862e958a0cf168a67c55
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2019-12-04 17:24:36 +0530

    Create interpolation.java

---
+++ Searching Algorithms/Interpolation Search/interpolation.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class interpolation{
+    public static void main(String[] args){
+        int first,last,mid=0,n,i,element,flag=0;
+        Scanner s=new Scanner(System.in);
+        System.out.print('\u000C');
+        System.out.println("Enter the number of elements you are going to enter:");
+        n=s.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter the elements in sorted order:");
+        for(i=0;i<n;i++){
+            arr[i]=s.nextInt();
+        }
+        System.out.println("Enter the element that you wish to search:");
+        element=s.nextInt();
+        first=0;
+        last=n-1;
+        while(arr[first]<=element && arr[last]>=element){
+            mid=first+(((element-arr[first])*(last-first))/(arr[last]-arr[first]));
+            if(arr[mid]==element){
+                flag=1;
+                break;
+            }
+            else if(element>arr[mid]){
+                first=mid+1;
+            }
+            else{
+                last=mid-1;
+            }
+        }
+        if(flag==1){
+            System.out.println("Element is present at "+(mid+1)+" position");
+        }
+        else{
+            System.out.println("Element is not present");
+        }
+    }
+}
