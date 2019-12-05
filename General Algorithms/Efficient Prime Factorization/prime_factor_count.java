842176372a0413ed1e43f536af6cc126b0f09853
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2019-12-05 11:40:34 +0530

    Create prime_factor_count.java

---
+++ General Algorithms/Efficient Prime Factorization/prime_factor_count.java
@@ -0,0 +1,35 @@
+// Java Program to calculate the number of Prime Factors of every element in an array of size 'n'
+
+import java.util.Scanner;
+public class prime_factor_array{
+    static int count(int n){
+        int flag=0;
+        while(n%2==0){
+            flag++;
+            n=n/2;
+        }
+        for(int i=3;i<Math.sqrt(n);i++){
+            while(n%i==0){
+                flag++;
+            }
+        }
+        if(n>2){ flag++; }
+        return flag;   
+    }
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array:");
+        int n=s.nextInt();
+        int i;
+        int arr[]=new int[n];
+        System.out.println("Enter the elements in the array:");
+        for(i=0;i<n;i++){
+            arr[i]=s.nextInt();
+        }
+        System.out.println("The output is:");
+        for(i=0;i<n;i++){
+            System.out.println(count(arr[i]));
+        }
+    }
+}
+        
