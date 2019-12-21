0635c9ceeea14fb99f638e65e33420da51c68af3
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2019-12-21 19:58:12 +0530

    Create Binary Search Tree : Lowest Common Ancestor.java

---
+++ Problem Solving-Hackerrank/Binary Search Tree : Lowest Common Ancestor.java
@@ -0,0 +1,69 @@
+# https://www.hackerrank.com/challenges/binary-search-tree-lowest-common-ancestor/problem
+
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+    
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+
+	/*
+    class Node 
+    	int data;
+    	Node left;
+    	Node right;
+	*/
+	static Node lca(Node n, int v1, int v2) {
+    while (n != null) {
+        if (n.data > v1 && n.data > v2) {
+            n = n.left;
+        } else if (n.data < v1 && n.data < v2) {
+            n = n.right;
+        } else {
+            break;
+        } 
+    }
+    return n;
+}
+
+	public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        int v1 = scan.nextInt();
+      	int v2 = scan.nextInt();
+        scan.close();
+        Node ans = lca(root,v1,v2);
+        System.out.println(ans.data);
+    }	
+}
