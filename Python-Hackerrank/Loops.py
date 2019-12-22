bb6a3f6bbd52723863b4c6d855f94fb868466c64
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2019-12-22 16:46:29 +0530

    Create Loops.py

---
+++ Python-Hackerrank/Loops.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/python-loops/problem
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(0,n):
+        c=i*i
+        print("%s" %c)
+
+
+
