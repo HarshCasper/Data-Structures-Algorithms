c8be6984cf5291a855bebcdc45bba1e401f78f03
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2019-12-22 16:41:21 +0530

    Create Arithmetic Operators.py

---
+++ Python-Hackerrank/Arithmetic Operators.py
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    c=a+b
+    d=a-b
+    e=a*b
+    print(c)
+    print(d)
+    print(e)
+
