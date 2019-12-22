15bebafdcaa2af1d99d5a68ffacc3878ca0872e6
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2019-12-22 17:12:13 +0530

    Create String Split and Join.py

---
+++ Python-Hackerrank/String Split and Join.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/python-string-split-and-join/problem
+
+def split_and_join(line):
+    # write your code here
+    line=line.split(' ')
+    line='-'.join(line)
+    return line
+
