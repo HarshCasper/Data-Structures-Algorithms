cbced3a7bce1c33ed998995897ee3fd35b17cef8
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2019-12-22 17:26:54 +0530

    Create Validating Postal Codes.py

---
+++ Python-Hackerrank/Validating Postal Codes.py
@@ -0,0 +1,5 @@
+# https://www.hackerrank.com/challenges/validating-postalcode/problem
+
+regex_integer_in_range = r"^[1-9][\d]{5}$"	# Do not delete 'r'.
+regex_alternating_repetitive_digit_pair = r"(\d)(?=\d\1)"	# Do not delete 'r'.
+
