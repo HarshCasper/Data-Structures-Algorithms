7ac9a26d045395a04ad3876f2b52793aecddb8d2
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2019-12-22 16:45:00 +0530

    Create Python: Division.py

---
+++ Python-Hackerrank/Python: Division.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/python-division/problem
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    c=a//b;
+    d=a/b
+    print(c)
+    print(d)
+
