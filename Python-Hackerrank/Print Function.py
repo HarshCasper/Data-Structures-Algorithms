78b4abe18fec1c4369f12445b7dc387a123ad41a
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2019-12-22 16:53:08 +0530

    Create Print Function.py

---
+++ Python-Hackerrank/Print Function.py
@@ -0,0 +1,11 @@
+# https://www.hackerrank.com/challenges/python-print/problem
+
+if __name__ == '__main__':
+    n = int(input())
+    sum=0
+    for i in range(1,n+1):
+        if(i!=0):
+            print(i,end="")
+
+        
+
