2179bffe40ff368bc6fbe068a650593dce791c5f
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2019-12-29 21:13:26 +0530

    Create Java BigInteger.java

---
+++ Java-Hackerrank/Java BigInteger.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    java.math.BigInteger ai = sc.nextBigInteger();
+    java.math.BigInteger bi = sc.nextBigInteger();
+    java.math.BigInteger sum =ai.add(bi);
+    java.math.BigInteger mul =ai.multiply(bi);
+    System.out.println(sum);
+    System.out.println(mul);
+}
+}
+
