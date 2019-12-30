321facbb66628259e8bb843496e1e320f372c9bd
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2019-12-30 20:28:07 +0530

    Create Java Exception Handling.java

---
+++ Java-Hackerrank/Java Exception Handling.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class MyCalculator{
+    public static int power(int n, int p) throws Exception{
+        if(n < 0 || p < 0){
+            throw new Exception ("n or p should not be negative.");
+        } 
+        else if (n == 0 && p == 0){
+            throw new Exception ("n and p should not be zero.");
+        }
+        else {
+            return ((int)Math.pow(n,p));
+        }
+    }
+}
+
+public class Solution {
