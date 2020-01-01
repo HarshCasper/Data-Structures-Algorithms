269c23fe37eb8fdcd78b544e998d84c96dca799e
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2020-01-01 20:03:34 +0530

    Create List Comprehensions.py

---
+++ Python-Hackerrank/List Comprehensions.py
@@ -0,0 +1,4 @@
+# https://www.hackerrank.com/challenges/list-comprehensions/problem
+
+x, y, z, n = int(input()), int(input()), int(input()), int(input())
+print ([[a,b,c] for a in range(0,x+1) for b in range(0,y+1) for c in range(0,z+1) if a + b + c != n ])
