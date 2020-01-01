7daa25d3f16864d853a303eb4fbf6fe727b95b73
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2020-01-01 20:32:28 +0530

    Create The Minion Game.py

---
+++ Python-Hackerrank/The Minion Game.py
@@ -0,0 +1,20 @@
+# https://www.hackerrank.com/challenges/the-minion-game/problem
+
+def minion_game(string):
+    vowel =['A','E','I','O','U']
+    S=0
+    K=0
+    for i in range(len(string)):
+        if string[i] in vowel:
+            K+= len(string)-i
+        else:
+            S+=len(string)-i
+    if S>K:
+        print("Stuart"+" "+ "%d" % S)
+    elif K>S:
+        print("Kevin"+" "+'%d' % K)
+    else:
+        print("Draw")
+if __name__ == '__main__':
+    s = raw_input()
+    minion_game(s)
