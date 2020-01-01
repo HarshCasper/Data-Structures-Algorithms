7c005e5c24bddf3850da38646b28387b507bc569
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2020-01-01 20:22:50 +0530

    Create Find a string.py

---
+++ Python-Hackerrank/Find a string.py
@@ -0,0 +1,22 @@
+# https://www.hackerrank.com/challenges/find-a-string/problem
+
+def count_substring(string, sub_string):
+    count=0
+    #print(len(string),len(sub_string))
+    for i in range(0, len(string)-len(sub_string)+1):
+        if string[i] == sub_string[0]:
+            flag=1
+            for j in range (0, len(sub_string)):
+                if string[i+j] != sub_string[j]:
+                    flag=0
+                    break
+            if flag==1:
+                count += 1
+    return count
+
+if __name__ == '__main__':
+    string = raw_input().strip()
+    sub_string = raw_input().strip()
+    
+    count = count_substring(string, sub_string)
+    print count
