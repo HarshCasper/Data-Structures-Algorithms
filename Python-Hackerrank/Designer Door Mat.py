13232405228200e4c55d0d5af200e3c71788c3e7
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2020-01-02 21:04:44 +0530

    Create Designer Door Mat.py

---
+++ Python-Hackerrank/Designer Door Mat.py
@@ -0,0 +1,14 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+if __name__ == '__main__':
+        N, M = map(int, input().split(" "))
+
+        for i in range(N):
+                pattern = ".|."
+                if i < (N-1)/2:
+                        print((pattern * (2*i+1)).center(M, "-"))
+                elif i == (N-1)/2:
+                        print("WELCOME".center(M, "-"))
+                else:
+                        print((pattern * (2*(N-1-i)+1)).center(M, "-"))
+
