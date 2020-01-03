14479ef860476e11429d2641b070b6efbbd63672
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2020-01-03 15:08:24 +0530

    Create Alphabet Rangoli.py

---
+++ Python-Hackerrank/Alphabet Rangoli.py
@@ -0,0 +1,9 @@
+def print_rangoli(size):
+    alp = 'abcdefghijklmnopqrstuvwxyz'
+    for i in range(size-1,-size,-1):
+        temp = '-'.join(alp[size-1:abs(i):-1]+alp[abs(i):size])
+        print(temp.center(4*size-3,'-'))
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
