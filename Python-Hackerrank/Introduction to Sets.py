b16e1a489df3bd6323172df07145a2df07e796bf
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2020-01-03 16:05:21 +0530

    Create Introduction to Sets.py

---
+++ Python-Hackerrank/Introduction to Sets.py
@@ -0,0 +1,8 @@
+def average(array):
+    # your code goes here
+    return sum(set(array))/len(set(array))
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
