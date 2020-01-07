ec2ba114fe56b6d4db6b4ad9f6a6e93b4322e3b1
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2020-01-07 22:30:37 +0530

    Create Find Angle MBC.py

---
+++ Python-Hackerrank/Find Angle MBC.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from math import *
+
+ab = float(input())
+bc = float(input())
+print(str(int(round(degrees(atan(ab/bc)),0)))+'°')
