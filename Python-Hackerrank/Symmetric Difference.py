57b6d1feea71080332db70e013cee8ed3af10f25
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2020-01-11 20:25:15 +0530

    Create Symmetric Difference.py

---
+++ Python-Hackerrank/Symmetric Difference.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+a,b=(int(input()),input().split())
+c,d=(int(input()),input().split())
+x=set(b)
+y=set(d)
+p=y.difference(x)
+q=x.difference(y)
+r=p.union(q)
+print ('\n'.join(sorted(r, key=int)))
