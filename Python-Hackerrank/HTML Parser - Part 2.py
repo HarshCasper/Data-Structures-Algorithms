36338761f293fd6bb256ac05781de2dcec4a6f64
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2020-01-18 02:14:10 +0530

    Create HTML Parser - Part 2.py

---
+++ Python-Hackerrank/HTML Parser - Part 2.py
@@ -0,0 +1,23 @@
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+    def handle_comment(self,data):
+        if "\n" not in data:
+            print(">>> Single-line Comment")
+            print(data,end="")
+        else:
+            print(">>> Multi-line Comment")
+            print(data)
+    def handle_data(self,data):
+        if data != '\n':
+            print(">>> Data")
+            print(data)
+ 
+html = ""       
+for i in range(int(input())):
+    html += input().rstrip()
+    html += '\n'
+    
+parser = MyHTMLParser()
+parser.feed(html)
+parser.close()
