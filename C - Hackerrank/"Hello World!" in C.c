4dc3273779ebefc7586c3b7577754236889c795b
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2020-04-05 16:51:42 +0530

    Create "Hello World!" in C.c

---
+++ C - Hackerrank/"Hello World!" in C.c
@@ -0,0 +1,17 @@
+// https://www.hackerrank.com/challenges/hello-world-c/problem
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char s[100];
+    scanf("%[^\n]%*c", &s);
+  	printf ( "Hello, World!" "\n");
+  printf ("%s",s);
+
+  return 0;
+}
