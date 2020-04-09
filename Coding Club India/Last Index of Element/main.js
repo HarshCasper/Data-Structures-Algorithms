2207ddc9df644868f39751865786e090b9fdce5d
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2020-04-09 12:01:19 +0530

    Create main.js

---
+++ Coding Club India/Last Index of Element/main.js
@@ -0,0 +1,10 @@
+function lastIndexElement(arr,number){
+  return arr.lastIndexOf(number);
+}
+
+// Driver Code 
+
+var num=7;
+var arr=[86,-16,77,65,45,77,28];
+var number=77;
+console.log("The Last Index of Element is "+lastIndexElement(arr,number));
